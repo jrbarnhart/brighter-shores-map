5eafcf5d0711b801364431feeb25a5f0db18e66c
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-09 16:09:16 -0500

    Install and config Shadcn UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div className="bg-blue-500">
       <p>Hello Map!</p>
+      <Button className="bg-blue-50 text-black hover:bg-blue-200">
+        Click Me!
+      </Button>
     </div>
   );
 }
