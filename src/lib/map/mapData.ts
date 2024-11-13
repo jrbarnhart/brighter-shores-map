99907f529a5c8e0a11953a98f0f8ccada7ecf3c7
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-13 12:41:41 -0500

    Split map data into regions for better organization

--- src/lib/map/mapData.ts
+++ src/lib/map/mapData.ts
@@ -1,3 +1,5 @@
+import hopeportData from "./hopeportData";
+
 export type RoomData = {
   id: string;
   origin: [number, number];
@@ -5,9 +7,9 @@ export type RoomData = {
   type: "residence" | "shop" | "world" | "profession" | "obelisk" | "portal";
 };
 
-export type MapData = {
+export type RegionData = {
   /**
-   * Array of rooms, each with a label, origin, shape, and type.
+   * Array of rooms, each with a id, origin, path, and type.
    * - id: HTML id used to create path that represents this room.
    * - origin: The coordinates [x, y] of the room. Origin (0,0) is top-left corner of Captain Degreene's Office.
    * - path: An array of coordinates representing the room's shape.
@@ -16,388 +18,28 @@ export type MapData = {
   rooms: RoomData[];
 };
 
+export type MapData = {
+  regions: { hopeport: RegionData };
+};
+
+/*
+  Room Template:
+  {
+    id: "id",
+    origin: [0,0],
+    path: [
+      [0,0],
+    ],
+    type: "world"
+  } 
+*/
+
 /**
  * Data that represents the game world of Brighter Shores.
- * - rooms: Array of rooms, each with a label, origin, shape, and type.
+ * - regions: Array of regions representing the main areas of the game map.
  */
 const mapData: MapData = {
-  rooms: [
-    /*
-      Template:
-    {
-      id: "id",
-      origin: [0,0],
-      path: [
-        [0,0],
-      ],
-      type: "world"
-    } 
-    */
-    {
-      id: "captain-degreenes-office",
-      origin: [0, 0],
-      path: [
-        [0, 0],
-        [6, 0],
-        [6, 6],
-        [5, 6],
-        [5, 7],
-        [3, 7],
-        [3, 6],
-        [0, 6],
-      ],
-      type: "residence",
-    },
-    {
-      id: "training-ground",
-      origin: [-6, 7],
-      path: [
-        [0, 0],
-        [2, 0],
-        [2, 2],
-        [9, 2],
-        [9, 0],
-        [11, 0],
-        [11, 2],
-        [18, 2],
-        [18, 0],
-        [20, 0],
-        [20, 2],
-        [21, 2],
-        [21, 3],
-        [23, 3],
-        [23, 5],
-        [21, 5],
-        [21, 14],
-        [23, 14],
-        [23, 16],
-        [21, 16],
-        [21, 19],
-        [19, 19],
-        [19, 15],
-        [1, 15],
-        [1, 11],
-        [-1, 11],
-        [-1, 9],
-        [0, 9],
-      ],
-      type: "world",
-    },
-    {
-      id: "hopeport-portal-stone",
-      origin: [-13, -4],
-      path: [
-        [0, 0],
-        [9, 0],
-        [9, 11],
-        [7, 11],
-        [7, 9],
-        [0, 9],
-      ],
-      type: "portal",
-    },
-    {
-      id: "sparring-area",
-      origin: [-22, 12],
-      path: [
-        [0, 0],
-        [13, 0],
-        [13, 4],
-        [15, 4],
-        [15, 6],
-        [13, 6],
-        [13, 9],
-        [0, 9],
-      ],
-      type: "world",
-    },
-    {
-      id: "barracks",
-      origin: [10, -1],
-      path: [
-        [0, 0],
-        [12, 0],
-        [12, 6],
-        [6, 6],
-        [6, 7],
-        [4, 7],
-        [4, 8],
-        [2, 8],
-        [2, 7],
-        [0, 7],
-      ],
-      type: "residence",
-    },
-    {
-      id: "infirmary",
-      origin: [18, 9],
-      path: [
-        [0, 0],
-        [6, 0],
-        [6, 6],
-        [4, 6],
-        [4, 8],
-        [2, 8],
-        [2, 6],
-        [0, 6],
-        [0, 3],
-        [-1, 3],
-        [-1, 1],
-        [0, 1],
-      ],
-      type: "residence",
-    },
-    {
-      id: "quartermaster",
-      origin: [20, 17],
-      path: [
-        [0, 0],
-        [2, 0],
-        [2, 1],
-        [4, 1],
-        [4, 7],
-        [-2, 7],
-        [-2, 6],
-        [-3, 6],
-        [-3, 4],
-        [-2, 4],
-        [-2, 1],
-        [0, 1],
-      ],
-      type: "shop",
-    },
-    {
-      id: "stone-street",
-      origin: [12, 26],
-      path: [
-        [0, 0],
-        [4, 0],
-        [4, 1],
-        [7, 1],
-        [7, 4],
-        [5, 4],
-        [5, 9],
-        [7, 9],
-        [7, 11],
-        [5, 11],
-        [5, 16],
-        [3, 16],
-        [3, 17],
-        [1, 17],
-        [1, 16],
-        [-1, 16],
-        [-1, 11],
-        [-3, 11],
-        [-3, 9],
-        [-1, 9],
-        [-1, 4],
-        [-3, 4],
-        [-3, 1],
-        [0, 1],
-      ],
-      type: "world",
-    },
-    {
-      id: "delectable-dab-restaurant",
-      origin: [-5, 33],
-      path: [
-        [0, 0],
-        [13, 0],
-        [13, 2],
-        [14, 2],
-        [14, 4],
-        [13, 4],
-        [13, 6],
-        [0, 6],
-        [0, 3],
-        [-1, 3],
-        [-1, 1],
-        [0, 1],
-      ],
-      type: "residence",
-    },
-    {
-      id: "delectable-dab-kitchen",
-      origin: [-15, 27],
-      path: [
-        [0, 0],
-        [8, 0],
-        [8, 7],
-        [9, 7],
-        [9, 9],
-        [8, 9],
-        [8, 14],
-        [0, 14],
-      ],
-      type: "profession",
-    },
-    {
-      id: "apothecary",
-      origin: [22, 29],
-      path: [
-        [0, 0],
-        [10, 0],
-        [10, 10],
-        [-2, 10],
-        [-2, 8],
-        [-3, 8],
-        [-3, 6],
-        [-2, 6],
-        [-2, 4],
-        [0, 4],
-      ],
-      type: "profession",
-    },
-    {
-      id: "town-square",
-      origin: [12, 43],
-      path: [
-        [0, 0],
-        [4, 0],
-        [4, 1],
-        [7, 1],
-        [7, 3],
-        [5, 3],
-        [5, 8],
-        [6, 8],
-        [6, 9],
-        [7, 9],
-        [7, 11],
-        [6, 11],
-        [6, 12],
-        [5, 12],
-        [5, 13],
-        [1, 13],
-        [1, 14],
-        [-3, 14],
-        [-3, 13],
-        [-7, 13],
-        [-7, 8],
-        [-9, 8],
-        [-9, 6],
-        [-7, 6],
-        [-7, 1],
-        [0, 1],
-      ],
-      type: "world",
-    },
-    {
-      id: "melvs-fishing-supplies",
-      origin: [20, 42],
-      path: [
-        [0, 0],
-        [7, 0],
-        [7, 7],
-        [0, 7],
-        [0, 4],
-        [-1, 4],
-        [-1, 2],
-        [0, 2],
-      ],
-      type: "shop",
-    },
-    {
-      id: "brannof-boulevard",
-      origin: [-16, 48],
-      path: [
-        [0, 0],
-        [19, 0],
-        [19, 6],
-        [16, 6],
-        [16, 7],
-        [14, 7],
-        [14, 6],
-        [0, 6],
-        [0, 4],
-        [-1, 4],
-        [-1, 2],
-        [0, 2],
-      ],
-      type: "world",
-    },
-    {
-      id: "brannof-halls-hallway",
-      origin: [-21, 47],
-      path: [
-        [0, 0],
-        [2, 0],
-        [2, 1],
-        [4, 1],
-        [4, 7],
-        [-2, 7],
-        [-2, 5],
-        [-3, 5],
-        [-3, 3],
-        [-2, 3],
-        [-2, 1],
-        [0, 1],
-      ],
-      type: "residence",
-    },
-    {
-      id: "brannofs-chamber",
-      origin: [-23, 40],
-      path: [
-        [0, 0],
-        [6, 0],
-        [6, 6],
-        [4, 6],
-        [4, 7],
-        [2, 7],
-        [2, 6],
-        [0, 6],
-      ],
-      type: "residence",
-    },
-    {
-      id: "brannofs-dining-room",
-      origin: [-32, 48],
-      path: [
-        [0, 0],
-        [7, 0],
-        [7, 2],
-        [8, 2],
-        [8, 4],
-        [7, 4],
-        [7, 6],
-        [0, 6],
-      ],
-      type: "residence",
-    },
-    {
-      id: "hopeport-obelisk",
-      origin: [-2, 55],
-      path: [
-        [0, 0],
-        [2, 0],
-        [2, 2],
-        [3, 2],
-        [3, 3],
-        [4, 3],
-        [4, 5],
-        [5, 5],
-        [5, 11],
-        [4, 11],
-        [4, 12],
-        [3, 12],
-        [3, 13],
-        [-7, 13],
-        [-7, 12],
-        [-8, 12],
-        [-8, 11],
-        [-9, 11],
-        [-9, 4],
-        [-8, 4],
-        [-8, 3],
-        [-7, 3],
-        [-7, 2],
-        [-5, 2],
-        [-5, 1],
-        [0, 1],
-      ],
-      type: "obelisk",
-    },
-  ],
+  regions: { hopeport: hopeportData },
 };
 
 export default mapData;
