75f82e6efde5ae8f16d82b73aaffb4908ac02aaa
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-14 13:47:21 -0500

    Remove jsdoc for now and add label to room data

--- src/lib/map/mapData.ts
+++ src/lib/map/mapData.ts
@@ -2,19 +2,13 @@ import hopeportData from "./hopeportData";
 
 export type RoomData = {
   id: string;
+  label: string;
   origin: [number, number];
   path: Array<[number, number]>;
   type: "residence" | "shop" | "world" | "profession" | "obelisk" | "portal";
 };
 
 export type RegionData = {
-  /**
-   * Array of rooms, each with a id, origin, path, and type.
-   * - id: HTML id used to create path that represents this room.
-   * - origin: The coordinates [x, y] of the room. Origin (0,0) is top-left corner of Captain Degreene's Office.
-   * - path: An array of coordinates representing the room's shape.
-   * - type: The type of room, which can be "residence", "shop", or "world".
-   */
   rooms: RoomData[];
 };
 
@@ -34,10 +28,6 @@ export type MapData = {
   } 
 */
 
-/**
- * Data that represents the game world of Brighter Shores.
- * - regions: Array of regions representing the main areas of the game map.
- */
 const mapData: MapData = {
   regions: { hopeport: hopeportData },
 };
