6cbc9d7ed1a11ac101bf748f4f84bf65367031ec
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-15 15:40:21 -0500

    Pass labelsHidden state to MapSVG

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,7 @@ function App() {
       <MapSidebar setLabelsHidden={setLabelsHidden} />
       <main className="h-screen w-screen relative overflow-hidden">
         <SidebarTrigger className="absolute mt-3 ml-3 z-10 bg-neutral-50 h-12 w-12 border border-green-500" />
-
-        <MapSVG />
+        <MapSVG labelsHidden={labelsHidden} />
       </main>
     </SidebarProvider>
   );
