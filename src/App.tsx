0197ff39967ea69ebc0006625a760ea8b8874ca0
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-15 16:12:55 -0500

    Implement useMapState

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
 import MapSVG from "./components/map/MapSVG";
+import useMapState from "./components/map/useMapState";
 import MapSidebar from "./components/sidebar/MapSidebar";
 import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
 
 function App() {
-  const [labelsHidden, setLabelsHidden] = useState(false);
+  const mapState = useMapState();
 
   return (
     <SidebarProvider>
-      <MapSidebar setLabelsHidden={setLabelsHidden} />
+      <MapSidebar mapState={mapState} />
       <main className="h-screen w-screen relative overflow-hidden">
         <SidebarTrigger className="absolute mt-3 ml-3 z-10 bg-neutral-50 h-12 w-12 border border-green-500" />
-        <MapSVG labelsHidden={labelsHidden} />
+        <MapSVG mapState={mapState} />
       </main>
     </SidebarProvider>
   );
