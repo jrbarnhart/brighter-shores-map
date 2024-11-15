9dd24a143cffc69dc8dbf89bc23932af353807f7
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-14 19:34:51 -0500

    Add simple layout to sidebar with btn for labels

--- src/components/sidebar/MapSidebar.tsx
+++ src/components/sidebar/MapSidebar.tsx
@@ -1,9 +1,29 @@
-import { Sidebar, SidebarContent } from "../ui/sidebar";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
 
 export default function MapSidebar() {
   return (
     <Sidebar>
-      <SidebarContent />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Brighter Map</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>Labels</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
     </Sidebar>
   );
 }
