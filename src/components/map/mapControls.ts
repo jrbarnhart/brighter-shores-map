a5d21acfc314028b9720139006f74fbef165f908
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-15 15:39:47 -0500

    Fix fn by removing hidden instead of setting to false

--- src/components/map/mapControls.ts
+++ src/components/map/mapControls.ts
@@ -1,6 +1,10 @@
 export function setHiddenGroups(groups: SVGGElement[], value: boolean) {
   // Check if first label is hidden
   for (const g of groups) {
-    g.setAttribute("hidden", value.toString());
+    if (value) {
+      g.setAttribute("hidden", "true");
+    } else {
+      g.removeAttribute("hidden");
+    }
   }
 }
