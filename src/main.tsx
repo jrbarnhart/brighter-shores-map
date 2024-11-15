eb7885b372935583f1a6035426db3d4712a7e3b2
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-14 19:28:28 -0500

    Adjust trigger style and hide overlow in main

--- src/main.tsx
+++ src/main.tsx
@@ -9,8 +9,8 @@ createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <SidebarProvider>
       <MapSidebar />
-      <main className="relative">
-        <SidebarTrigger className="absolute z-10" />
+      <main className="relative overflow-hidden">
+        <SidebarTrigger className="absolute mt-3 ml-3 z-10 bg-neutral-50 h-12 w-12 border border-green-500" />
         <App />
       </main>
     </SidebarProvider>
