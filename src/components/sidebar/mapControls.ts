49021a3e4cd656290d0b0cf1a237032de6ff8117
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-15 15:40:42 -0500

    Moved file

--- src/components/sidebar/mapControls.ts
+++
@@ -1,7 +0,0 @@
-export function toggleGroups(groups: SVGGElement[]) {
-  // Check if first label is hidden
-  const isHidden = groups[0].getAttribute("hidden") === "true";
-  for (const g of groups) {
-    g.setAttribute("hidden", (!isHidden).toString());
-  }
-}
