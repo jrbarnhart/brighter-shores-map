3e584264d472ac1864b546fa0ed6a1a13171e788
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-15 15:41:03 -0500

    Add ref and useEffect for toggling label groups

--- src/components/map/MapSVG.tsx
+++ src/components/map/MapSVG.tsx
@@ -3,9 +3,18 @@ import { drawLabels, drawRooms } from "./drawMap";
 import initMap from "./initMap";
 import usePan from "./usePan";
 import useZoom from "./useZoom";
+import { setHiddenGroups } from "./mapControls";
 
-export default function MapSVG() {
+export default function MapSVG({ ...props }: { labelsHidden: boolean }) {
+  const { labelsHidden } = props;
   const svgRef = useRef<SVGSVGElement | null>(null);
+
+  const labelGroups = useRef<SVGGElement[]>([]);
+
+  // Hide labels on state change
+  useEffect(() => {
+    setHiddenGroups(labelGroups.current, labelsHidden);
+  }, [labelsHidden]);
 
   const {
     handleMouseDown,
@@ -24,7 +33,8 @@ export default function MapSVG() {
     if (!svgRef.current.hasAttribute("data-drawn")) {
       const initMapValues = initMap(svgRef.current);
       drawRooms(svgRef.current, initMapValues);
-      drawLabels(svgRef.current, initMapValues);
+      const labelGroupElements = drawLabels(svgRef.current, initMapValues);
+      labelGroups.current = labelGroupElements;
       svgRef.current.setAttribute("data-drawn", "true");
     }
   }, []);
