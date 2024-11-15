0c5d8bc73b96be9141ce3cccc711ceef9e6d7c4e
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-15 15:20:30 -0500

    Add state for labels being hidden

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import MapSVG from "./components/map/MapSVG";
 import MapSidebar from "./components/sidebar/MapSidebar";
 import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
 
 function App() {
+  const [labelsHidden, setLabelsHidden] = useState(false);
+
   return (
     <SidebarProvider>
-      <MapSidebar />
+      <MapSidebar setLabelsHidden={setLabelsHidden} />
       <main className="h-screen w-screen relative overflow-hidden">
         <SidebarTrigger className="absolute mt-3 ml-3 z-10 bg-neutral-50 h-12 w-12 border border-green-500" />
 
