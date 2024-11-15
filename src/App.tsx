aae0e8e05b1a1d5948d2565845d5cd1c095e99e8
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-15 14:52:15 -0500

    Move sidebar stuff to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import MapSVG from "./components/map/MapSVG";
+import MapSidebar from "./components/sidebar/MapSidebar";
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
 
 function App() {
   return (
-    <div className="h-screen w-screen">
-      <MapSVG />
-    </div>
+    <SidebarProvider>
+      <MapSidebar />
+      <main className="h-screen w-screen relative overflow-hidden">
+        <SidebarTrigger className="absolute mt-3 ml-3 z-10 bg-neutral-50 h-12 w-12 border border-green-500" />
+
+        <MapSVG />
+      </main>
+    </SidebarProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar.tsx";
-import MapSidebar from "./components/sidebar/MapSidebar.tsx";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <SidebarProvider>
-      <MapSidebar />
-      <main className="relative overflow-hidden">
-        <SidebarTrigger className="absolute mt-3 ml-3 z-10 bg-neutral-50 h-12 w-12 border border-green-500" />
-        <App />
-      </main>
-    </SidebarProvider>
+    <App />
   </StrictMode>
 );
