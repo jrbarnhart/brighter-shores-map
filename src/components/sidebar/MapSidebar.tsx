44e0239dbeea938f60aecfbb5fb008c15da59ade
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-14 19:07:31 -0500

    Add blank simple sidebar with trigger

---
+++ src/components/sidebar/MapSidebar.tsx
@@ -0,0 +1,9 @@
+import { Sidebar, SidebarContent } from "../ui/sidebar";
+
+export default function MapSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent />
+    </Sidebar>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar.tsx";
+import MapSidebar from "./components/sidebar/MapSidebar.tsx";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <App />
+    <SidebarProvider>
+      <MapSidebar />
+      <main className="relative">
+        <SidebarTrigger className="absolute z-10" />
+        <App />
+      </main>
+    </SidebarProvider>
   </StrictMode>
 );
