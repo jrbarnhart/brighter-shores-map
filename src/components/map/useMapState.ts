221cb07305032215554a6151ee53fa51419b80b9
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-16 00:15:31 -0500

    Add scale state

--- src/components/map/useMapState.ts
+++ src/components/map/useMapState.ts
@@ -5,13 +5,19 @@ export type MapState = {
     value: boolean;
     set: React.Dispatch<SetStateAction<boolean>>;
   };
+  scale: {
+    value: number;
+    set: React.Dispatch<SetStateAction<number>>;
+  };
 };
 
 export default function useMapState() {
   const [labelsHidden, setLabelsHidden] = useState(false);
+  const [scale, setScale] = useState(100);
 
   const mapState: MapState = {
     labelsHidden: { value: labelsHidden, set: setLabelsHidden },
+    scale: { value: scale, set: setScale },
   };
 
   return mapState;
