3ad36e7e209c00490b4e0721e1fb8bca88b7e701
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-18 21:47:00 -0500

    Add monsters to content data

--- src/lib/map/hopeport/hopeportContentData.ts
+++ src/lib/map/hopeport/hopeportContentData.ts
@@ -16,6 +16,26 @@ export type HopeportRoomContentData = {
 
 const hopeportContentData = [
   { roomId: "sparring-area", monsters: ["guard"] },
+  { roomId: "town-gates", monsters: ["deathcrow"] },
+  { roomId: "old-street-east", monsters: ["deathcrow"] },
+  { roomId: "tims-alley", monsters: ["thief"] },
+  { roomId: "limpet-lane", monsters: ["thief"] },
+  { roomId: "north-walls", monsters: ["goblin solider"] },
+  { roomId: "south-walls", monsters: ["goblin chief"] },
+  { roomId: "overgrown-road", monsters: ["hairbeast"] },
+  { roomId: "rubble-road", monsters: ["hairbeast"] },
+  { roomId: "overgrown-field", monsters: ["carnivorous plant"] },
+  { roomId: "fallen-monument", monsters: ["carnivorous plant"] },
+  { roomId: "collapsed-hovels", monsters: ["ghoul"] },
+  { roomId: "wasteland", monsters: ["ghoul"] },
+  { roomId: "wilhope-passage", monsters: ["street louse"] },
+  { roomId: "murk-close", monsters: ["thug"] },
+  { roomId: "east-beach", monsters: ["crab"] },
+  { roomId: "overgrown-pond", monsters: ["bugman"] },
+  { roomId: "jellyfish-landing", monsters: ["jellyfish"] },
+  { roomId: "crater", monsters: ["giant ant"] },
+  { roomId: "pirate-hideout", monsters: ["pirate"] },
+  { roomId: "rocky-road", monsters: ["punkupine"] },
 ] as const satisfies readonly HopeportRoomContentData[];
 
 export default hopeportContentData;
