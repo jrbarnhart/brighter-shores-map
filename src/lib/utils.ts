a087319aa580cfc45eea356997cd3eb6495e5fa8
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-21 15:10:25 -0500

    Add cancel method to debounced fns

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,10 +8,16 @@ export function cn(...inputs: ClassValue[]) {
 export function debounce(func: (...args: unknown[]) => void, timeout = 300) {
   let timer: ReturnType<typeof setTimeout>;
 
-  return (...args: unknown[]) => {
+  const debouncedFn = (...args: unknown[]) => {
     clearTimeout(timer);
     timer = setTimeout(() => {
       func(...args);
     }, timeout);
   };
+
+  debouncedFn.cancel = () => {
+    clearTimeout(timer);
+  };
+
+  return debouncedFn;
 }
