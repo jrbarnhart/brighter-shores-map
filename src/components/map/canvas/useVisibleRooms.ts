b8660d2b6ecc4b686737af317d81c9c2e6fb6602
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-21 02:18:05 -0500

    Set visible rooms to found room paths

--- src/components/map/canvas/useVisibleRooms.ts
+++ src/components/map/canvas/useVisibleRooms.ts
@@ -4,8 +4,8 @@ import { useEffect } from "react";
 import mapConfig from "@/lib/map/mapConfig";
 
 export default function useVisibleRooms({ mapState }: { mapState: MapState }) {
-  const { rTree, canvas, mapPos, visibleRooms } = mapState;
-  const setVisibleRooms = visibleRooms.set;
+  const { rTree, canvas, mapPos, visibleRoomPaths, roomPaths } = mapState;
+  const setVisibleRooms = visibleRoomPaths.set;
   useEffect(() => {
     const roomsCanvas = canvas.rooms.ref.current;
     if (!roomsCanvas || !rTree.value) return;
@@ -17,9 +17,13 @@ export default function useVisibleRooms({ mapState }: { mapState: MapState }) {
       maxY: roomsCanvas.height / mapConfig.cellSize - mapPos.value.y,
     };
 
-    const result = rTree.value
+    const foundRoomIds = rTree.value
       .search(bbox)
       .map((foundNode) => foundNode.roomId);
+
+    const result = roomPaths.value.filter((path) =>
+      foundRoomIds.includes(path.roomId)
+    );
 
     setVisibleRooms(result);
   }, [
@@ -27,6 +31,7 @@ export default function useVisibleRooms({ mapState }: { mapState: MapState }) {
     mapPos.value.x,
     mapPos.value.y,
     rTree.value,
+    roomPaths.value,
     setVisibleRooms,
   ]);
 }
