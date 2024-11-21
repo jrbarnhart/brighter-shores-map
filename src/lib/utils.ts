da2cf86b0283da6808d05541b0b2350bd4934994
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-21 15:45:23 -0500

    Remove debounce util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,20 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function debounce(func: (...args: unknown[]) => void, timeout = 300) {
-  let timer: ReturnType<typeof setTimeout>;
-
-  const debouncedFn = (...args: unknown[]) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func(...args);
-    }, timeout);
-  };
-
-  debouncedFn.cancel = () => {
-    clearTimeout(timer);
-  };
-
-  return debouncedFn;
-}
