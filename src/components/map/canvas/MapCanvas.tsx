acccfa5d8feaf8dd6da9deb7d8b63a89d78b50cd
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-21 15:46:40 -0500

    Set canvas size based on state

--- src/components/map/canvas/MapCanvas.tsx
+++ src/components/map/canvas/MapCanvas.tsx
@@ -1,4 +1,5 @@
 import { MapState } from "../useMapState";
+import useCanvasSize from "./useCanvasSize";
 import useCreateRoomPaths from "./useCreateRoomPaths";
 import useCreateRTree from "./useCreateRTree";
 import useDrawRooms from "./useDrawRooms";
@@ -7,6 +8,9 @@ import useVisibleRooms from "./useVisibleRooms";
 export default function MapCanvas({ mapState }: { mapState: MapState }) {
   const { canvas } = mapState;
   const roomCanvasRef = canvas.rooms.ref;
+  const roomCanvasSize = canvas.rooms.size.value;
+  // Set canvas size
+  useCanvasSize({ mapState });
   // Computes path2D's and set them to state
   useCreateRoomPaths({ setRoomPaths: mapState.roomPaths.set });
   // Creates an rtree of objects with min/max x/y and room id
@@ -16,7 +20,14 @@ export default function MapCanvas({ mapState }: { mapState: MapState }) {
   // Draw the visible rooms
   useDrawRooms({ mapState });
 
-  return <canvas ref={roomCanvasRef} className="w-screen h-screen"></canvas>;
+  return (
+    <canvas
+      ref={roomCanvasRef}
+      height={roomCanvasSize.height}
+      width={roomCanvasSize.width}
+      className="bg-blue-500"
+    ></canvas>
+  );
 }
 
 // This draw function should only run when needed (zoom or panning the map for example)
