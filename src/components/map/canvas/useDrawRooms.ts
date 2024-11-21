744556c6c2d39448525f01208d46fe8e01e5b033
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-21 16:31:17 -0500

    Clear the canvas before drawing

--- src/components/map/canvas/useDrawRooms.ts
+++ src/components/map/canvas/useDrawRooms.ts
@@ -1,30 +1,44 @@
 import { useEffect } from "react";
 import { RoomPathData } from "./useCreateRoomPaths";
 import { MapState } from "../useMapState";
+import mapConfig from "@/lib/map/mapConfig";
 
 export default function useDrawRooms({ mapState }: { mapState: MapState }) {
   function drawRoomPaths(
     ctx: CanvasRenderingContext2D,
-    roomPaths: RoomPathData[]
+    roomPaths: RoomPathData[],
+    mapPos: { x: number; y: number },
+    cellSize: number
   ): void {
+    ctx.save();
+
+    // Subtract the mapPos values to move content opposite to drag direction
+    ctx.translate(-mapPos.x * cellSize, -mapPos.y * cellSize);
+
     roomPaths.forEach((roomPath) => {
-      // Set the fill color for the current room
       ctx.fillStyle = roomPath.fillColor;
-
-      // Fill the path directly
       ctx.fill(roomPath.element);
-
-      // Optional: Add a stroke if you want room outlines
       ctx.strokeStyle = "black";
       ctx.stroke(roomPath.element);
     });
+
+    ctx.restore();
   }
 
   useEffect(() => {
-    const roomCanvasContext =
+    const mapPos = mapState.mapPos.value;
+    const { cellSize } = mapConfig;
+    const roomsCanvas = mapState.canvas.rooms.ref.current;
+    const roomsCanvasContext =
       mapState.canvas.rooms.ref.current?.getContext("2d");
     const visibleRooms = mapState.visibleRoomPaths.value;
-    if (!roomCanvasContext || visibleRooms.length === 0) return;
-    drawRoomPaths(roomCanvasContext, visibleRooms);
-  }, [mapState.canvas.rooms.ref, mapState.visibleRoomPaths.value]);
+    if (!roomsCanvasContext || !roomsCanvas || visibleRooms.length === 0)
+      return;
+    roomsCanvasContext.clearRect(0, 0, roomsCanvas.width, roomsCanvas.height);
+    drawRoomPaths(roomsCanvasContext, visibleRooms, mapPos, cellSize);
+  }, [
+    mapState.canvas.rooms.ref,
+    mapState.mapPos.value,
+    mapState.visibleRoomPaths.value,
+  ]);
 }
