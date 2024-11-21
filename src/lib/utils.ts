7ea0e5739b9fca9345bcee3d196042a8c69ff211
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-21 15:03:45 -0500

    Add debounce util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function debounce(func: (...args: unknown[]) => void, timeout = 300) {
+  let timer: ReturnType<typeof setTimeout>;
+
+  return (...args: unknown[]) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, timeout);
+  };
 }
