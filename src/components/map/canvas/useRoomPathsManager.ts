59df9b2e3e73f0f1ba3128a9565a828b5cbf1744
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-22 17:24:11 -0500

    Combine logic for room paths into single manager

---
+++ src/components/map/canvas/useRoomPathsManager.ts
@@ -0,0 +1,99 @@
+import { useMemo } from "react";
+import { MapState } from "../useMapState";
+import { RoomPathData } from "./useCreateRoomPaths";
+import mapData, { RoomId } from "@/lib/map/mapData";
+import mapConfig from "@/lib/map/mapConfig";
+import RBush, { BBox } from "rbush";
+import { RoomTreeNode } from "./useCreateRTree";
+
+function createCanvasPath2D(
+  path: [number, number][],
+  origin: [number, number],
+  cellSize: number
+) {
+  const canvasPath = new Path2D();
+
+  if (path.length === 0) return canvasPath;
+
+  // Multiply startX/Y by cell size
+  const startX = (path[0][0] + origin[0]) * cellSize;
+  const startY = (path[0][1] + origin[1]) * cellSize;
+
+  canvasPath.moveTo(startX, startY);
+
+  for (let i = 1; i < path.length; i++) {
+    const x = (path[i][0] + origin[0]) * cellSize;
+    const y = (path[i][1] + origin[1]) * cellSize;
+    canvasPath.lineTo(x, y);
+  }
+
+  canvasPath.closePath();
+  return canvasPath;
+}
+
+function createCanvasPaths(currentCellSize: number) {
+  const roomPaths: RoomPathData[] = [];
+  for (const region of Object.values(mapData.regions)) {
+    for (const room of region.rooms) {
+      const roomPath = {
+        element: createCanvasPath2D(room.path, room.origin, currentCellSize),
+        roomId: room.id as RoomId,
+        fillColor: room.color ?? mapConfig.defaultRoomFill,
+      };
+      roomPaths.push(roomPath);
+    }
+  }
+  return roomPaths;
+}
+
+function filterVisiblePaths(
+  roomPaths: RoomPathData[],
+  roomsCanvas: HTMLCanvasElement | null,
+  currentCellSize: number,
+  mapPos: { x: number; y: number },
+  rTree: RBush<RoomTreeNode> | undefined
+) {
+  if (!roomsCanvas || !rTree) return [];
+  // Convert canvas dimensions to normalized cell space
+  const canvasWidthInCells = roomsCanvas.width / currentCellSize;
+  const canvasHeightInCells = roomsCanvas.height / currentCellSize;
+
+  const bbox: BBox = {
+    minX: mapPos.x,
+    minY: mapPos.y,
+    maxX: mapPos.x + canvasWidthInCells,
+    maxY: mapPos.y + canvasHeightInCells,
+  };
+
+  const foundRoomIds = rTree.search(bbox).map((foundNode) => foundNode.roomId);
+
+  const result = roomPaths.filter((path) => foundRoomIds.includes(path.roomId));
+  return result;
+}
+
+export default function useRoomPathsManager({
+  mapState,
+}: {
+  mapState: MapState;
+}) {
+  const currentCellSize = mapState.currentCellSize.value;
+  const roomCanvas = mapState.canvas.rooms.ref.current;
+  const mapPos = mapState.mapPos.value;
+  const rTree = mapState.rTree.value;
+
+  const fullPaths = useMemo(() => {
+    return createCanvasPaths(currentCellSize);
+  }, [currentCellSize]);
+
+  const visiblePaths = useMemo(() => {
+    return filterVisiblePaths(
+      fullPaths,
+      roomCanvas,
+      currentCellSize,
+      mapPos,
+      rTree
+    );
+  }, [currentCellSize, fullPaths, mapPos, rTree, roomCanvas]);
+
+  return { fullPaths, visiblePaths };
+}
