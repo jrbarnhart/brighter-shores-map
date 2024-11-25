e112c09dfb0b5e02f50d61a96acc59d1b58a1abf
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-25 15:32:25 -0500

    Add util for converting to px values from normalized

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+type NormalizedValue = number;
+type PixelValue = number;
+export function toPixels(value: NormalizedValue, cellSize: number): PixelValue {
+  return value * cellSize;
+}
