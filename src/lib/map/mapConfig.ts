1f9657e14497f593a0ac386ba015539261df7034
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-11-25 15:52:59 -0500

    Add pixel/normalized value types for clarity

--- src/lib/map/mapConfig.ts
+++ src/lib/map/mapConfig.ts
@@ -1,16 +1,18 @@
+import { NormalizedValue, PixelValue } from "../generalTypes";
+
 const mapConfig = {
-  defaultCellSize: 20,
-  maxCellSize: 30,
-  minCellSize: 5,
+  defaultCellSize: 20 as PixelValue,
+  maxCellSize: 30 as PixelValue,
+  minCellSize: 5 as PixelValue,
   cellSizeIncrement: 5,
   dragDeltaMod: 0.1,
   doubleTouchThreshold: 300,
   bgColor: "#2a2b2a",
   defaultColor: "#c5b57b",
   defaultRoomFill: "#c5b57b",
-  labelPadding: 2,
-  labelMaxLineWidth: 4,
-  labelLineHeight: 0.8,
+  labelPadding: 2 as NormalizedValue,
+  labelMaxLineWidth: 4 as NormalizedValue,
+  labelLineHeight: 0.8 as NormalizedValue,
 } as const;
 
 export type MapConfig = typeof mapConfig;
