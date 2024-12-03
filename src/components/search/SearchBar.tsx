6319ba0ae9518308d9c36df49b377af2267e6598
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-03 15:42:11 -0500

    Implement fuse.js fuzzy search to set search.results

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,25 +1,40 @@
-import { useEffect, useRef } from "react";
+import { SetStateAction, useEffect, useMemo, useRef } from "react";
 import { MapState } from "../map/useMapState";
 import useDebounce from "@/hooks/useDebounce";
+import Fuse from "fuse.js";
+import { searchableData } from "@/lib/map/mapData";
+import { SearchResult } from "@/lib/types";
 
 export default function SearchBar({ mapState }: { mapState: MapState }) {
   const { search } = mapState;
 
   const searchInputRef = useRef<HTMLInputElement>(null);
 
+  const fuse = useMemo(
+    () =>
+      new Fuse(searchableData, { keys: ["name", "variants"], threshold: 0.3 }),
+    []
+  );
+
   // Define debounced search query handler
-  const handleSearchQueryChange = useDebounce(() => {
-    // Take the value in search query and
-    // Do the search lmao
-    console.log("Debounced");
-  }, 300);
+  const handleSearchQueryChange = useDebounce(
+    (
+      query: string,
+      setResults: React.Dispatch<SetStateAction<SearchResult[]>>
+    ) => {
+      const results = fuse.search(query);
+      setResults(results.map((result) => result.item));
+      console.log("Debounced");
+    },
+    300
+  );
 
   useEffect(() => {
-    handleSearchQueryChange();
+    handleSearchQueryChange(search.query.value, search.results.set);
     return () => {
       handleSearchQueryChange.cancel();
     };
-  }, [handleSearchQueryChange, search.query.value]);
+  }, [handleSearchQueryChange, search.query.value, search.results.set]);
 
   return (
     <div className="absolute top-0 z-10 right-0 mt-3 bg-green-500/50 w-full flex justify-center h-12">
