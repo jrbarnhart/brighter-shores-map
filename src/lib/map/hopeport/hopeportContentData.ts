75e1d48a99143f9c1f2bb63a9619d7b7beec5170
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-06 14:55:37 -0500

    Create content data progrommatically to ensure single authority

--- src/lib/map/hopeport/hopeportContentData.ts
+++ src/lib/map/hopeport/hopeportContentData.ts
@@ -1,6 +1,8 @@
 import { BankType, NPC } from "@/lib/types";
 import { HopeportRoomId } from "./hopeportRoomData";
-import { HopeportMonsterName } from "./roomContents/hopeportMonsterData";
+import HOPEPORT_MONSTER_DATA, {
+  HopeportMonsterName,
+} from "./roomContents/hopeportMonsterData";
 
 export type HopeportRoomContentData = {
   roomId: HopeportRoomId;
@@ -13,28 +15,35 @@ export type HopeportRoomContentData = {
   banks?: BankType[];
 };
 
+// Create the monster content data
+const newHopeportContentData: HopeportRoomContentData[] = [];
+// Add all locations from all monsters
+for (const monster of HOPEPORT_MONSTER_DATA) {
+  for (const location of monster.locations) {
+    // Check for existing room entry
+    const existingRoomEntry = newHopeportContentData.find(
+      (data) => data.roomId === location
+    );
+    // If there is no entry yet add it
+    if (!existingRoomEntry) {
+      newHopeportContentData.push({
+        roomId: location,
+        monsters: [monster.name],
+      });
+      // Else if the room entry exists and somehow has no monsters array
+    } else if (!existingRoomEntry.monsters) {
+      existingRoomEntry.monsters = [monster.name];
+      // Else the room entry exists and has monsters array so add to it
+    } else {
+      existingRoomEntry.monsters.push(monster.name);
+    }
+  }
+}
+
+// TODO: Add the resource and npc entries here later on
+
 const hopeportContentData = [
-  { roomId: "sparring-area", monsters: ["guard"] },
-  { roomId: "town-gates", monsters: ["deathcrow"] },
-  { roomId: "old-street-east", monsters: ["deathcrow"] },
-  { roomId: "tims-alley", monsters: ["thief"] },
-  { roomId: "limpet-lane", monsters: ["thief"] },
-  { roomId: "north-walls", monsters: ["goblin solider"] },
-  { roomId: "south-walls", monsters: ["goblin chief"] },
-  { roomId: "overgrown-road", monsters: ["hairbeast"] },
-  { roomId: "rubble-road", monsters: ["hairbeast"] },
-  { roomId: "overgrown-field", monsters: ["carnivorous plant"] },
-  { roomId: "fallen-monument", monsters: ["carnivorous plant"] },
-  { roomId: "collapsed-hovels", monsters: ["ghoul"] },
-  { roomId: "wasteland", monsters: ["ghoul"] },
-  { roomId: "wilhope-passage", monsters: ["street louse"] },
-  { roomId: "murk-close", monsters: ["thug"] },
-  { roomId: "east-beach", monsters: ["crab"] },
-  { roomId: "overgrown-pond", monsters: ["bugman"] },
-  { roomId: "jellyfish-landing", monsters: ["jellyfish"] },
-  { roomId: "crater", monsters: ["giant ant"] },
-  { roomId: "pirate-hideout", monsters: ["pirate"] },
-  { roomId: "rocky-road", monsters: ["punkupine"] },
+  ...newHopeportContentData,
 ] as const satisfies readonly HopeportRoomContentData[];
 
 export default hopeportContentData;
