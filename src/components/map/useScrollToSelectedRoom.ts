ae6de1be5b7327e985ddde1fe75c3769f7cd7d25
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-06 16:29:23 -0500

    Adjust the scroll by an offset based on window size

--- src/components/map/useScrollToSelectedRoom.ts
+++ src/components/map/useScrollToSelectedRoom.ts
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { MapState } from "./useMapState";
-import { findRoomById } from "@/lib/utils";
+import { findRoomById, toNormalizedGridSpace } from "@/lib/utils";
 
 export default function useScrollToSelectedRoom({
   mapState,
@@ -10,14 +10,25 @@ export default function useScrollToSelectedRoom({
   const { selectedRoomId, mapPos } = mapState;
   const setMapPos = mapPos.set;
   const rooms = mapState.roomPaths.value;
+  const currentCellSize = mapState.currentCellSize.value;
 
   useEffect(() => {
     if (selectedRoomId.value) {
       const roomPath = findRoomById(rooms, selectedRoomId.value);
       if (roomPath) {
-        const { x, y } = roomPath.center;
+        const offX = toNormalizedGridSpace(
+          window.innerWidth / 2,
+          currentCellSize
+        );
+        const offY = toNormalizedGridSpace(
+          window.innerHeight / 2,
+          currentCellSize
+        );
+        let { x, y } = roomPath.center;
+        x -= offX;
+        y -= offY;
         setMapPos({ x, y });
       }
     }
-  }, [rooms, selectedRoomId.value, setMapPos]);
+  }, [currentCellSize, rooms, selectedRoomId.value, setMapPos]);
 }
