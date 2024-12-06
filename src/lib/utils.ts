1067076f5da5dec2922e3173bc8834f2ef21d522
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-06 16:06:28 -0500

    Add util for finding room data by id

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,17 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { NormalizedValue, PixelValue, Size } from "./types";
+import { MapData, NormalizedValue, PixelValue, RoomId, Size } from "./types";
 import { SetStateAction } from "react";
 
+// Tailwind class merging
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// Functions to convert between pixel space and normalized grid space
 export function toPixels(value: NormalizedValue, cellSize: number): PixelValue {
   return value * cellSize;
 }
-
 export function toNormalizedGridSpace(
   value: PixelValue,
   cellSize: number
@@ -18,6 +19,7 @@ export function toNormalizedGridSpace(
   return Math.round(value / cellSize);
 }
 
+// Used by various control interfaces to zoom the map
 export function adjustMapPosOnZoom(
   setMapPos: React.Dispatch<
     SetStateAction<{ x: NormalizedValue; y: NormalizedValue }>
@@ -45,4 +47,11 @@ export function adjustMapPosOnZoom(
 
     return { x: newX, y: newY };
   });
+}
+
+// Find the room data by a given room id
+export function findRoomById(mapData: MapData, roomId: RoomId) {
+  return Object.values(mapData.regions)
+    .flatMap((region) => region.rooms)
+    .find((room) => room.id === roomId);
 }
