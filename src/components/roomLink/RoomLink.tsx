821f04a37770fbff224d314e54e2d5ee24a75005
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-06 14:06:30 -0500

    Add simple room link component that sets state

---
+++ src/components/roomLink/RoomLink.tsx
@@ -0,0 +1,23 @@
+import { RoomId } from "@/lib/types";
+import React, { SetStateAction } from "react";
+
+export default function RoomLink({
+  ...props
+}: {
+  text: string;
+  roomId: RoomId;
+  setSelectedRoomId: React.Dispatch<SetStateAction<RoomId>>;
+}) {
+  const { text, roomId, setSelectedRoomId } = props;
+
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setSelectedRoomId(roomId);
+  };
+
+  return (
+    <a onClick={handleClick} href={`/${roomId}`}>
+      {text}
+    </a>
+  );
+}
