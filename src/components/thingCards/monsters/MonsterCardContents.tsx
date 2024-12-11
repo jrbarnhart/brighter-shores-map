2db458c61a4fb445975054a06d3aa38fd73a1030
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-11 18:18:29 -0500

    Add expanded context and elements

--- src/components/thingCards/monsters/MonsterCardContents.tsx
+++ src/components/thingCards/monsters/MonsterCardContents.tsx
@@ -1,15 +1,22 @@
 import { Monster } from "@/lib/types";
 import { Impact, Necromae, Tempestae } from "../../gameIcons/gameIcons";
+import useThingCardContext from "../useThingCardContext";
 
 export default function MonsterCardContents({ monster }: { monster: Monster }) {
+  const { expandedCardId } = useThingCardContext();
+  const isExpanded = monster.name === expandedCardId;
+
   return (
-    <div className="h-10 flex items-center justify-between gap-1 text-sm">
-      <p>Attack: </p>
-      <Impact />
-      <p>Immune: </p>
-      <Tempestae />
-      <p>Weak: </p>
-      <Necromae />
-    </div>
+    <>
+      <div className="h-12 flex items-center gap-1 md:gap-3 text-sm max-w-md">
+        <p>Attack: </p>
+        <Impact />
+        <p>Immune: </p>
+        <Tempestae />
+        <p>Weak: </p>
+        <Necromae />
+      </div>
+      {isExpanded && <div>Expanded!</div>}
+    </>
   );
 }
--- src/components/thingCards/monsters/MonsterCardHeader.tsx
+++ src/components/thingCards/monsters/MonsterCardHeader.tsx
@@ -4,6 +4,7 @@ import { CardDescription, CardTitle } from "@/components/ui/card";
 import { Monster } from "@/lib/types";
 import { findRoomById } from "@/lib/utils";
 import React from "react";
+import useThingCardContext from "../useThingCardContext";
 
 export default function MonsterCardHeader({
   monster,
@@ -13,25 +14,31 @@ export default function MonsterCardHeader({
   mapState: MapState;
 }) {
   const { selectedRoomId } = mapState;
+  const { expandedCardId } = useThingCardContext();
+  const isExpanded = monster.name === expandedCardId;
+
   return (
     <>
       <CardTitle>
         {monster.name[0].toUpperCase() + monster.name.slice(1)}
       </CardTitle>
-      <CardDescription>
-        {monster.locations.map((location, index) => {
-          const room = findRoomById(mapState.roomPaths.value, location);
-          return (
-            <React.Fragment key={index}>
-              <RoomLink
-                text={room ? room.label : location}
-                roomId={location}
-                setSelectedRoomId={selectedRoomId.set}
-              />
-              {monster.locations.length - 1 > index ? ", " : ""}
-            </React.Fragment>
-          );
-        })}
+      <CardDescription className="flex flex-col">
+        {isExpanded && <p>Locations:</p>}
+        <div>
+          {monster.locations.map((location, index) => {
+            const room = findRoomById(mapState.roomPaths.value, location);
+            return (
+              <React.Fragment key={index}>
+                <RoomLink
+                  text={room ? room.label : location}
+                  roomId={location}
+                  setSelectedRoomId={selectedRoomId.set}
+                />
+                {monster.locations.length - 1 > index ? ", " : ""}
+              </React.Fragment>
+            );
+          })}
+        </div>
       </CardDescription>
     </>
   );
