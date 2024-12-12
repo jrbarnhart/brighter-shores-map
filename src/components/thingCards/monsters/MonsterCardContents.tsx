66fa34c03cf023a943c0bb5d149382bfec05a6a2
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-11 20:00:10 -0500

    Fix missing key

--- src/components/thingCards/monsters/MonsterCardContents.tsx
+++ src/components/thingCards/monsters/MonsterCardContents.tsx
@@ -1,6 +1,7 @@
 import { Monster } from "@/lib/types";
 import useThingCardContext from "../useThingCardContext";
 import { getDamageIcon } from "@/components/gameIcons/gameIconUtils";
+import React from "react";
 
 export default function MonsterCardContents({ monster }: { monster: Monster }) {
   const { expandedCardId } = useThingCardContext();
@@ -25,18 +26,18 @@ export default function MonsterCardContents({ monster }: { monster: Monster }) {
         )}
       </div>
       {isExpanded && (
-        <div className="border-sidebar-border border rounded-md p-2 -mx-2 flex-grow overflow-y-auto overflow-x-hidden pointer-events-none">
+        <div className="border-sidebar-border border rounded-md p-2 -mx-2 flex-grow overflow-y-auto overflow-x-hidden">
           <div className="grid grid-cols-[3fr_1fr_1fr_1fr_1fr] gap-x-2 border-b-2 border-sidebar-border">
             <p>Variant:</p> <p>Health</p> <p>XP</p> <p>Lvl</p> <p>🔒 Lvl</p>
           </div>
           <div className="grid grid-cols-[3fr_1fr_1fr_1fr_1fr] gap-x-2 text-sm">
             {monster.variants.map((variant) => (
-              <>
+              <React.Fragment key={variant.name}>
                 <p>{variant.name[0].toUpperCase() + variant.name.slice(1)}</p>
                 <p>{variant.health}</p> <p>{variant.experience}</p>{" "}
                 <p>{variant.monsterLevel}</p>
                 <p>{variant.unlockLevel}</p>
-              </>
+              </React.Fragment>
             ))}
           </div>
         </div>
