c9e52a7092561cc9a5be117537e8cf59324f7be6
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-12 15:06:58 -0500

    Adjust style for md screens

--- src/components/thingCards/ThingCard.tsx
+++ src/components/thingCards/ThingCard.tsx
@@ -59,7 +59,7 @@ export default function ThingCard({
             <X />
           </div>
         )}
-        <CardHeader className="px-3 py-2 md:p-6">
+        <CardHeader className="px-3 py-2 md:p-4">
           {thing.type === "monster" && (
             <MonsterCardHeader
               monster={thing}
@@ -68,7 +68,7 @@ export default function ThingCard({
             />
           )}
         </CardHeader>
-        <CardContent className="px-3 pb-1 md:px-6 md:pb-3 flex flex-col gap-4 flex-grow overflow-hidden">
+        <CardContent className="px-3 pb-1 md:px-4 md:pb-2 flex flex-col gap-4 flex-grow overflow-hidden">
           {thing.type === "monster" && <MonsterCardContents monster={thing} />}
         </CardContent>
       </Card>
