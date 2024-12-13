e6d01737669a71345d2013e208adaaa5c25f3ec2
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-13 16:19:12 -0500

    Simplify expanded card logic

--- src/components/expandedCard/ExpandedCard.tsx
+++ src/components/expandedCard/ExpandedCard.tsx
@@ -22,24 +22,16 @@ export default function ExpandedCard({ mapState }: { mapState: MapState }) {
     [expandedCardThing.value]
   );
 
-  return (
-    <div
-      onClick={handleClick}
-      className={
-        expandedCardThing.value
-          ? "w-full h-full fixed top-0 left-0 flex items-center justify-center touch-none bg-black/80 backdrop-blur-sm"
-          : ""
-      }
-    >
-      <Card
-        onClick={handleCardClick}
-        className={`${
-          expandedCardThing.value
-            ? "w-screen max-w-[640px] h-screen max-h-[360px] m-5 scale-105"
-            : "scale-100"
-        } bg-sidebar border-sidebar-border text-sidebar-foreground transition-transform ease-in flex flex-col`}
+  if (expandedCardThing.value) {
+    return (
+      <div
+        onClick={handleClick}
+        className="w-full h-full fixed top-0 left-0 flex items-center justify-center touch-none bg-black/80 backdrop-blur-sm"
       >
-        {expandedCardThing.value && (
+        <Card
+          onClick={handleCardClick}
+          className="w-screen max-w-[640px] h-screen max-h-[360px] m-5 scale-105 bg-sidebar border-sidebar-border text-sidebar-foreground transition-transform ease-in flex flex-col"
+        >
           <div
             onClick={handleClick}
             className="absolute -top-7 right-0 flex gap-2"
@@ -47,24 +39,24 @@ export default function ExpandedCard({ mapState }: { mapState: MapState }) {
             <p>Close</p>
             <X />
           </div>
-        )}
-        <CardHeader className="px-3 py-2 md:p-4">
-          {expandedCardThing.value &&
-            expandedCardThing.value.type === "monster" && (
+
+          <CardHeader className="px-3 py-2 md:p-4">
+            {expandedCardThing.value.type === "monster" && (
               <MonsterCardHeader
                 monster={expandedCardThing.value}
                 mapState={mapState}
                 key={expandedCardThing.value.name}
               />
             )}
-        </CardHeader>
-        <CardContent className="px-3 pb-1 md:px-4 md:pb-2 flex flex-col gap-4 flex-grow overflow-hidden">
-          {expandedCardThing.value &&
-            expandedCardThing.value.type === "monster" && (
+          </CardHeader>
+          <CardContent className="px-3 pb-1 md:px-4 md:pb-2 flex flex-col gap-4 flex-grow overflow-hidden">
+            {expandedCardThing.value.type === "monster" && (
               <MonsterCardContents monster={expandedCardThing.value} />
             )}
-        </CardContent>
-      </Card>
-    </div>
-  );
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+  return null;
 }
