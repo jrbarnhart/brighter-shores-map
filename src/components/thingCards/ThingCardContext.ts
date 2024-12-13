d888976a5bb1586d5f1a7ed3d3548e413d9e53ba
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-13 17:02:17 -0500

    Remove old thing card files

--- src/components/thingCards/ThingCardContext.ts
+++
@@ -1,7 +0,0 @@
-import { ThingCardContextType } from "@/lib/types";
-import { createContext } from "react";
-
-export const ThingCardContext = createContext<ThingCardContextType>({
-  expandedCardId: null,
-  setExpandedCardId: () => {},
-});
