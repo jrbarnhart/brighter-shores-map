70e9998c4bbf3a4052836847c6c15710346a7067
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-12-14 18:17:38 -0500

    Take the monster instead of the base name

--- src/components/monsterLink/MonsterLink.tsx
+++ src/components/monsterLink/MonsterLink.tsx
@@ -1,30 +1,26 @@
-import { findMonsterByBaseName } from "@/lib/map/mapDataUtils";
-import { MonsterBaseName, Thing } from "@/lib/types";
+import { Monster, Thing } from "@/lib/types";
 import React, { SetStateAction } from "react";
 
 export default function MonsterLink({
   ...props
 }: {
   text: string;
-  monsterBaseName: MonsterBaseName;
+  monster: Monster | null;
   setExpandedCardThing: React.Dispatch<SetStateAction<Thing | null>>;
 }) {
-  const { text, monsterBaseName, setExpandedCardThing } = props;
+  const { text, monster, setExpandedCardThing } = props;
 
   const handleClick = (e: React.MouseEvent) => {
     e.preventDefault();
     e.stopPropagation();
-    const monsterSearch = findMonsterByBaseName(monsterBaseName);
-    setExpandedCardThing(
-      monsterSearch.result ? { type: "monster", ...monsterSearch.result } : null
-    );
+    setExpandedCardThing(monster ? { type: "monster", ...monster } : null);
   };
 
   return (
     <a
       className="underline text-sidebar-accent"
       onClick={handleClick}
-      href={`/${monsterBaseName}`}
+      href={`/${monster?.name ?? ""}`}
     >
       {text}
     </a>
