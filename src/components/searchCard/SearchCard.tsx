47ccf78eef4e55054b5979704de3ed25f7940d87
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-08 20:20:40 -0500

    Add sub components for resource results

--- src/components/searchCard/SearchCard.tsx
+++ src/components/searchCard/SearchCard.tsx
@@ -7,12 +7,64 @@ import {
   CardTitle,
 } from "../ui/card";
 import { MapState } from "../map/useMapState";
-import { Monster, Thing } from "@/lib/types";
+import { Monster, ResourceNode, Thing } from "@/lib/types";
 import React from "react";
 import RoomLink from "../roomLink/RoomLink";
 import { Passive } from "../gameIcons/gameIcons";
 import { getDamageIcon } from "../gameIcons/gameIconUtils";
 import { findRoomById } from "@/lib/map/mapDataUtils";
+
+const ResourceCardHeader = ({
+  mapState,
+  resource,
+}: {
+  mapState: MapState;
+  resource: ResourceNode;
+}) => {
+  const { selectedRoomId } = mapState;
+
+  return (
+    <>
+      <CardTitle className="flex flex-nowrap justify-between items-start">
+        <p className="text-nowrap overflow-hidden leading-8">
+          {resource.baseName[0].toUpperCase() + resource.baseName.slice(1)}
+        </p>
+      </CardTitle>
+      <CardDescription className="flex flex-col">
+        <div>
+          {resource.locations.map((location, index) => {
+            const room = findRoomById(mapState.roomPaths.value, location);
+            return (
+              <React.Fragment key={index}>
+                <RoomLink
+                  text={room ? room.label : location}
+                  roomId={location}
+                  setSelectedRoomId={selectedRoomId.set}
+                />
+                {resource.locations.length - 1 > index ? ", " : ""}
+              </React.Fragment>
+            );
+          })}
+        </div>
+      </CardDescription>
+    </>
+  );
+};
+
+const ResourceCardContents = ({ resource }: { resource: ResourceNode }) => {
+  return (
+    <>
+      <div className="h-5 flex items-center gap-1 md:gap-3 text-sm max-w-md">
+        {resource.passive && (
+          <>
+            <p>Passive</p>
+            <Passive />
+          </>
+        )}
+      </div>
+    </>
+  );
+};
 
 const MonsterCardHeader = ({
   mapState,
@@ -109,15 +161,15 @@ export default function SearchCard({
     >
       <CardHeader className="px-3 py-2 md:p-4">
         {thing.type === "monster" && (
-          <MonsterCardHeader
-            monster={thing}
-            mapState={mapState}
-            key={thing.name}
-          />
+          <MonsterCardHeader monster={thing} mapState={mapState} />
+        )}
+        {thing.type === "resource" && (
+          <ResourceCardHeader resource={thing} mapState={mapState} />
         )}
       </CardHeader>
       <CardContent className="px-3 pb-1 md:px-4 md:pb-2 flex flex-col gap-4 flex-grow overflow-hidden">
         {thing.type === "monster" && <MonsterCardContents monster={thing} />}
+        {thing.type === "resource" && <ResourceCardContents resource={thing} />}
       </CardContent>
     </Card>
   );
